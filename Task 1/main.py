2ec55aec2d07f5596e579f265525536cb5caf662
Arseniy Ott <arseniy.ott@mail.ru>
2024-06-17 15:41:20 +0200

    Update main.py

--- Task 1/main.py
+++ Task 1/main.py
@@ -1,49 +1,28 @@
-from typing import List
-import vk_api
-
-
-class VKCommentsFetcher:
-    def __init__(self, service_token: str, user_id: int) -> None:
-        self._service_token = service_token
-        self._user_id = user_id
-        self._vk = vk_api.VkApi(token=self._service_token).get_api()
-        self._user_comments = []
-
-    def _get_mentions(self) -> dict:
-        return self._vk.newsfeed.getMentions(owner_id=self._user_id, access_token=self._service_token)
-
-    def _get_comments_response(self, community_id: int, post_id: int) -> List[dict]:
-        return self._vk.wall.getComments(owner_id=community_id, post_id=post_id)
-
-
-    def _generate_user_comments(self) -> None:
-        mention = self._get_mentions()
-        for post in mention['items']:
-            comments_response = self._get_comments_response(post['to_id'], post['id'])
-            comments = [comment['text'] for comment in comments_response.get('items', []) if comment['from_id'] == self._user_id]
-            self._user_comments.extend(comments)
-
-    def get_user_comments(self) -> List[str]:
-        try:
-            self._generate_user_comments()
-
-        except vk_api.exceptions.ApiError as error:
-            print(f"Error: {error}")
-
-        return self._user_comments
+import os
+import sys
+from src.VKFetcher.VKCommentsFetcher import VKCommentsFetcher
+from src.ml.ml_Emotional_Analysis import EmotionalAnalysisComments
 
 
 def main():
-    with open('credentials/key', 'r') as file:
-        service_token = file.read()
+    current_dir = os.path.dirname(sys.executable)
+    credentials_path = os.path.abspath(os.path.join(current_dir, '..', 'credentials', 'key'))
+
+    with open(credentials_path, 'r') as file:
+        service_token = file.read().strip()
 
     user_id = int(input("Enter the user ID: "))
 
     fetcher = VKCommentsFetcher(service_token, user_id)
     comments = fetcher.get_user_comments()
 
-    for comment in comments:
-        print(comment)
+    model_path = os.path.abspath(os.path.join(current_dir, '..', 'src', 'ml', 'trainer', 'emotion_model.joblib'))
+    vectorizer_path = os.path.abspath(os.path.join(current_dir, '..', 'src', 'ml', 'trainer', 'vectorizer.joblib'))
+
+    EmotionMessage = EmotionalAnalysisComments(model_path, vectorizer_path, comments)
+    EmotionMessage.analysis_comments()
+
+    input("Press any button to exit...")
 
 
 if __name__ == "__main__":
